4fa475d9c67bac28d80416db8466afe604920ab1
Eric <40010382+sinbinsam@users.noreply.github.com>
2019-12-10 22:19:17 -0500

    building index

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
-var express = require('express');
 var app = express();
-var loki = require('lokijs')
+var loki = require('lokijs');
+var routes = require('./public/js/routes.js');
 var configdb = new loki('config');
 
 app.set('view engine', 'ejs');
@@ -12,17 +12,21 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(express.static(__dirname + '/public')); //stylesheets and js
+app.use('/', routes); //routes file
 
 
 
-app.get('/', (req, res) => {
-    res.render("index")
-})
 
 
 
 
 
 // start the server
-app.listen(port);
-console.log('Server started! V2 At http://localhost:' + port);
+app.listen(port, (err) => {
+    if (err) {
+        console.log('there was an error starting the server')
+        console.log(err)
+        } else {
+            console.log('Server started! V2 At http://localhost:' + port);
+        }
+});
